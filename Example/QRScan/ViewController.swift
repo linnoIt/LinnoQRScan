49232af15fd2a2cdded18b210def31a543a1c225
hanzc <it@linno.cn>
2022-09-22 14:37:32 +0800

    add test

--- Example/QRScan/ViewController.swift
+++ Example/QRScan/ViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import QRScan
 
 class ViewController: UIViewController {
-
+    var kQR:QRProxy?
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .systemGray
+        let or =  CGPoint(x: view.center.x - (view.bounds.width - 50)/2, y: view.center.y - (view.bounds.width - 50)/2)
+        kQR = QRProxy.init(bounds:CGRect(origin: or, size: CGSize(width: view.bounds.width - 50, height: view.bounds.width - 50)), showView: self.view, outPut: { kResult in
+            print(kResult as Any)
+        })
         // Do any additional setup after loading the view, typically from a nib.
     }
 
